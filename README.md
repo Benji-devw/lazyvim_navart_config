1b8c2f24a7f31239262a2df46e6ee0f5c28f3caf
Navart <ben.nav@pm.me>
2025-03-19 13:08:35 +0100

    update keymaps & readme

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+1. Clone repo to same nvim path
+```bash
+git clone <URL-du-répertoire>
+```
+
+2. Save your nvim folder
+3. Rename lazyvim_navart_config to nvim 
+4. Run nvim
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,8 +8,16 @@ vim.keymap.set({ "n", "v" }, "<leader>)", ":Gen<CR>")
 -- Remapper la touche "à" pour passer à l'onglet suivant
 -- vim.api.nvim_set_keymap("n", "à", ":tabnext<CR>", { noremap = true, silent = true })
 
+-- Cursor move
 vim.keymap.set("n", "<M-j>", "5j", { noremap = true, silent = true }) -- Cursor up 5 ligne
 vim.keymap.set("n", "<M-k>", "5k", { noremap = true, silent = true }) -- Cursor down 5 ligne
-
 vim.keymap.set("n", "<M-h>", "^", { noremap = true, silent = true }) -- End ligne
 vim.keymap.set("n", "<M-l>", "$", { noremap = true, silent = true }) -- Start ligne
+
+-- Move ligne
+vim.keymap.set("n", "<M-Down>", ":m .+1<CR>==", { noremap = true, silent = true }) -- Déplace la ligne vers le bas
+vim.keymap.set("n", "<M-Up>", ":m .-2<CR>==", { noremap = true, silent = true }) -- Déplace la ligne vers le haut
+vim.keymap.set("v", "<M-Down>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true }) -- Déplace la sélection vers le bas
+vim.keymap.set("v", "<M-Up>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true }) -- Déplace la sélection vers le haut
+vim.keymap.set("i", "<M-Down>", "<Esc>:m .+1<CR>==gi", { noremap = true, silent = true }) -- Déplace la ligne en mode insertion (bas)
+vim.keymap.set("i", "<M-Up>", "<Esc>:m .-2<CR>==gi", { noremap = true, silent = true }) -- Déplace la ligne en mode insertion (haut)
